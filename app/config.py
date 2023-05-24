baef9d7872ee9bacdf33b032ddd7a9df29b33dc8
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-05-24 17:31:19 +0300

    v.2.1

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Configuration(object):
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2RTY4356@ktldb_db_1/ktl_db' # подключаем базу данных
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:2RTY4356@db-1/ktl_db' # подключаем базу данных
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SECURITY_PASSWORD_SALT = 'fthydn'
 	SECURITY_PASSWORD_HASH = 'sha256_crypt'
